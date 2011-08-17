93db6ec61cb8dcea2ecb1f24696098033ad71d25
Lyle Hanson <lyle2.0@gmail.com>
2011-08-17 17:30:23 -0500

    Laying out items on main page

--- src/cognition_caps/handlers.clj
+++ src/cognition_caps/handlers.clj
@@ -1,10 +1,16 @@
 (ns cognition-caps.handlers
   (:use cognition-caps.data.simpledb)
   (:require [cognition-caps.data :as data]
-            ;[cognition-caps.data.simpledb] ;:only (simpledb)]
             [net.cgrand.enlive-html :as html]))
 
+; Snippet to generate item markup
+(html/defsnippet item-model "index.html" [:#items :.item] [cap]
+  [:.itemName] (html/content (:nom cap))
+  [:.itemPrice] (html/content "$666")
+  [:img] (html/set-attr :src (first (:image-urls cap))))
+
 (html/deftemplate index "index.html" [ctx]
+  [:#items] (html/content (map item-model (:caps ctx)))
   ; The last thing we do is to set the elapsed time
   [:#requestStats]
     (html/content (str "Response generated in "
@@ -14,4 +20,4 @@
 (defn root [request]
   (let [query-count (get-in request [:stats :db-queries])
         caps (data/get-caps simpledb query-count)]
-    (index (merge (:stats request) {:db-queries query-count}))))
+    (index (merge {:caps (take 7 caps)} (:stats request)))))
