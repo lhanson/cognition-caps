f3599a0a988f0c2d80b98af2ccbdc60cd8a6bff3
Lyle Hanson <lyle2.0@gmail.com>
2012-02-11 11:49:22 -0600

    Highlighting the selected thumbnail

--- resources/public/cognition.js
+++ resources/public/cognition.js
@@ -1,4 +1,5 @@
 $(document).ready(function() {
+    /* Thumbnail toggling for item pages */
     var $thumbnails = $("#thumbnails img");
     if ($thumbnails.length) {
         var $mainImage = $("#itemImageWrapper img");
@@ -6,7 +7,12 @@ $(document).ready(function() {
             .css("cursor", "hand").css("cursor", "pointer")
             .each(function(i, e) {
                 var $img = $(e);
+                if (i === 0) {
+                    $img.addClass("selected");
+                }
                 $img.click(function() {
+                    $thumbnails.removeClass("selected");
+                    $img.addClass("selected");
                     $mainImage.attr("src", $img.attr("src").replace("thumb", "main"));
                 });
             });
