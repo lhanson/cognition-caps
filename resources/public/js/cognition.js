e1dd6219cb8f56deb2a2669a62e238cc6d38909e
Lyle Hanson <lyle2.0@gmail.com>
2012-02-12 12:02:43 -0600

    Optimizing JavaScript loading and beginning to implement Modernizr polyfills

---
+++ resources/public/js/cognition.js
@@ -0,0 +1,47 @@
+$(document).ready(function() {
+    /* Regressive enhancement to support HTML5 input elements */
+    Modernizr.load({
+        test: Modernizr.input.placeholder &&
+              Modernizr.input.required &&
+              Modernizr.inputtypes.email,
+        nope: 'js/polyfill/jquery.html5form-1.5-min.js',
+        complete: function() {
+            if ($.fn.html5form) {
+                alert("Loaded polyfill for HTML5 form validation");
+                //TODO: is this necessary to activate? $('#emailForm').html5form();
+            }
+        }
+    });
+
+    /* Thumbnail toggling for item pages */
+    var $thumbnails = $('#thumbnails img');
+    if ($thumbnails.length) {
+        var $mainImage = $('#itemImageWrapper img');
+        $thumbnails
+            .css('cursor', 'hand').css('cursor', 'pointer')
+            .each(function(i, e) {
+                var $img = $(e);
+                if (i === 0) {
+                    $img.addClass('selected');
+                }
+                $img.click(function() {
+                    $thumbnails.removeClass('selected');
+                    $img.addClass('selected');
+                    $mainImage.attr('src', $img.attr('src').replace('thumb', 'main'));
+                });
+            });
+    }
+
+    /* Overlay for sizing chart */
+    $('#sizingLink').click(function(e) {
+        $.get('/sizing', function(data) {
+            $('body').append(
+                $('<div class="overlay" />')
+                    .append($(data).find('#sizing'))
+                    .height($(document).height())
+            );
+        });
+        e.preventDefault();
+    });
+});
+
